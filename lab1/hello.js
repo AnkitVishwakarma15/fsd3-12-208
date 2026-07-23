b9f7be04bc1d06ac6e54b9ca19092b19708de2aa
Ankit Vishwakarma <ankit0vishwakarma0@gmail.com>
2026-07-23 11:53:30 +0530

    intial commit

---
+++ lab1/hello.js
@@ -0,0 +1,13 @@
+function sum(a,b){
+    return a+b;
+}
+
+const add = (a,b) => {
+    return a + b;
+};
+
+const add2=(a, b) => a + b ;
+
+console.log(sum(10,10));
+console.log(add(10,10));
+console.log(add2(10,10));
