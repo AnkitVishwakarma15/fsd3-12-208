057bd7093300e2281b0606a71efc8f30ac599252
Ankit Vishwakarma <ankit0vishwakarma0@gmail.com>
2026-09-10 11:55:42 +0530

    REST api

--- lab3/prg6.js
+++ lab3/prg6.js
@@ -2,9 +2,17 @@ import { createReadStream } from "fs";
 import http from "http";
 
 const server = http.createServer((req, res) => {
-  if (req.url === "/") res.end("<h1>Home Page");
-  else if (req.url === "/product") {
+  res.setHeader("content-type", "text/html");
+  if (req.url === "/") {
+    res.write("<h1>home page</h1>");
+    res.end(`
+      <a href ="/product"> Product</a>
+      <a href="/contact">Contact Us</a>`);
+  } else if (req.url === "/product") {
     const stream = createReadStream("product.html", { encoding: "utf-8" });
+    stream.pipe(res);
+  } else if (req.url === "/contact") {
+    const stream = createReadStream("contact.html", { encoding: "utf-8" });
     stream.pipe(res);
   } else {
     res.statusCode = 404;
