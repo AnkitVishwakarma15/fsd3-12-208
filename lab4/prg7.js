29b250cbf75ca33a69ab048cfb78a574d91db465
Ankit Vishwakarma <ankit0vishwakarma0@gmail.com>
2026-09-17 12:21:55 +0530

    add user and get user by id created

--- lab4/prg7.js
+++ lab4/prg7.js
@@ -18,16 +18,16 @@ const server = http.createServer((req, res) => {
     });
     res.end(JSON.stringify({ msg: "add user" }));
   } 
-  else if (req.url === "/api/users/1" && req.method === "GET") {
-
-    
-    res.end(JSON.stringify({ msg: "single user with id 1" }));
+  else if (req.url.startsWith("/api/users/") && req.method === "GET") {
+    const userId = Number(req.url.split("/").pop());
+    res.end(JSON.stringify({ msg: `showing details for user with id ${userId}`
+    }));
   } 
   else if (req.url === "/api/users/1" && req.method === "PUT") {
     res.end(JSON.stringify({ msg: "update user with id 1" }));
   } 
   else if (req.url === "/api/users/1" && req.method === "DELETE") {
-    res.end(JSON.stringify({ msg: "remove user with id 1" }));
+    res.end(JSON.stringify({ msg: "remove 1" }));
   } 
   else {
     res.statusCode = 404;
