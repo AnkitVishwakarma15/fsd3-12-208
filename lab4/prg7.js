3e82a44e1797640cfd3db655b8ace9281e0ac120
Ankit Vishwakarma <ankit0vishwakarma0@gmail.com>
2026-09-17 12:16:26 +0530

    "Add user "

--- lab4/prg7.js
+++ lab4/prg7.js
@@ -1,26 +1,38 @@
 import http from "http";
-import { getUsers } from "./users.js";
-const server = http.createServer((req, res) => {
+import { addUser, getUsers } from "./users.js";
 
-    if (req.url === '/api/users' && req.method === 'GET') {
-        res.end(JSON.stringify(getUsers()));
-    }
-    else if (req.url === '/api/users' && req.method === 'POST') {
-        res.end(JSON.stringify({ msg:"add user" }));
-    }
-    else if (req.url === '/api/users/1' && req.method === 'GET') {
-        res.end(JSON.stringify({ msg:"single user with id 1" }));
-    }
-    else if (req.url === '/api/users/1' && req.method === 'PUT') {
-        res.end(JSON.stringify({ msg:"update user with id 1" }));
-    }
-    else if (req.url === '/api/users/1' && req.method === 'DELETE') {
-        res.end(JSON.stringify({ msg:"remove user with id 1" }));
-    }
-    else {
-        res.statusCode = 404;
-        res.end();
-    }
+const server = http.createServer((req, res) => {
+  if (req.url === "/api/users" && req.method === "GET") {
+    res.end(JSON.stringify(getUsers()));
+  }
+   else if (req.url === "/api/users" && req.method === "POST") {
+    let body = "";
+    req.on("data", (chunk) => {
+      body += chunk;
+    });
+    req.on("end", () => {
+      const user = JSON.parse(body);
+      const userCreated = addUser(user);
+
+      res.end(JSON.stringify({ msg: "user added", userCreated }));
+    });
+    res.end(JSON.stringify({ msg: "add user" }));
+  } 
+  else if (req.url === "/api/users/1" && req.method === "GET") {
+
+    
+    res.end(JSON.stringify({ msg: "single user with id 1" }));
+  } 
+  else if (req.url === "/api/users/1" && req.method === "PUT") {
+    res.end(JSON.stringify({ msg: "update user with id 1" }));
+  } 
+  else if (req.url === "/api/users/1" && req.method === "DELETE") {
+    res.end(JSON.stringify({ msg: "remove user with id 1" }));
+  } 
+  else {
+    res.statusCode = 404;
+    res.end();
+  }
 });
 
 server.listen(3000, () => console.log("server is running at 3000..."));
